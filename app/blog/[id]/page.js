fea2da3cfc88b4c071fccda8c0814b00125bf9e5
shahariar270 <mdshahariar102@gmail.com>
2026-07-14 07:11:25 +0600

    Fetch and display blog post content dynamically based on ID

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -8,6 +8,8 @@ export const metadata = {
 
 export default async function BlogPost({ params }) {
     const { id } = await params;
+    const currentData = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    const currentPost = await currentData.json();
 
     // Fetch the blog post data based on the ID
     // This is a placeholder - replace with actual data fetching logic
@@ -19,8 +21,8 @@ export default async function BlogPost({ params }) {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-4xl font-bold mb-4">{blogPost.title}</h1>
-            <p className="text-lg text-gray-600">{blogPost.content}</p>
+            <h1 className="text-4xl font-bold mb-4">{currentPost.title}</h1>
+            <p className="text-lg text-gray-600">{currentPost.body}</p>
         </div>
     );
 }
--- app/blog/page.js
+++ app/blog/page.js
@@ -10,14 +10,14 @@ export const metadata = {
 export default async function Blog() {
     const data = await fetch('https://jsonplaceholder.typicode.com/posts')
     const post = await data.json();
-    console.log({ post });
+
     return (
         <div className="flex justify-center items-center gap-5 flex-col">
             {post.map((item, index) => (
                 <div key={index} className="bg-blue-700 flex-col h-6 w-fit p-6 flex justify-center items-center text-white font-bold ">
                     <h1>{item.title}</h1>
                     <button>
-                    <Link href={`/blog/${item.id}`}>Read post</Link>
+                        <Link href={`/blog/${item.id}`}>Read post</Link>
                     </button>
                 </div>
             ))}
